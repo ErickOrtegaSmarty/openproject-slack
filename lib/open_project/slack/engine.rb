df62f8ac14ed230055743c0145989ada27797d09
Erick Ortega <erik.ortega@smartydreams.com>
2022-04-06 13:13:19 -0500

    Plugin working on docker

--- lib/open_project/slack/engine.rb
+++ lib/open_project/slack/engine.rb
@@ -15,20 +15,18 @@ module OpenProject::Slack
     ) do
       menu :project_menu,
            :slack,
-           { controller: '/slack', action: 'index', id: :openproject_slack },
+           { controller: '/slack', action: 'index'},
            caption: :label_slack_plugin,
            icon: 'icon2 icon-slack'
     
            project_module :slack do |_map|
             permission :view_slack, slack: %i[index show download]
-          end
-        end
+           end
+           Redmine::Search.register :slack
+      end
 
-    initializer 'slack.register_hooks' do
-      require 'open_project/slack/hook_listener'
+    add_api_path :slack do
+      "#{root}/slack"
     end
-    add_api_path :documents do
-      "#{root}/documents"
     end
   end
-end
